4613b85cc23e5380fcf575af0958e1f84e9ad2a2
Jossue <jossuearteche000@gmail.com>
2023-09-13 23:19:51 -0400

    Corregida la descripcion de la clase SimilarityResult

--- MoogleEngine/SimilarityResult.cs
+++ MoogleEngine/SimilarityResult.cs
@@ -3,8 +3,8 @@ using System.Runtime.CompilerServices;
 
 public class SimilarityResult : IComparable
 {
-	//esta clase es un contenedor mongo de dos variables, sirve para lo mismo que una tupla,
-	//solo que es mas bonito acceder a las cosas por su nombre que por item1 o item 2.
+	//esta clase es un contenedor de dos variables, cumple el mismo proposito que una tupla,
+	//solo que es mas entendible acceder a las cosas por su nombre que por item1 o item 2.
 	public SimilarityResult(double value, int index)
 	{
 		Value = value;
